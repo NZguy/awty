03c395b60e539a2f15aaedb2f1bfddb6092e145d
Duncan Andrew <duncanandrew@live.com>
2017-02-21 22:59:00 -0800

    Removed custom text watcher

--- app/src/main/java/drma/washington/edu/thereyet/MainActivity.java
+++ app/src/main/java/drma/washington/edu/thereyet/MainActivity.java
@@ -3,6 +3,7 @@ package drma.washington.edu.thereyet;
 import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
+import android.telephony.PhoneNumberFormattingTextWatcher;
 import android.telephony.PhoneNumberUtils;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -26,43 +27,6 @@ public class MainActivity extends Activity {
         inputMessage = (EditText) findViewById(R.id.inputMessage);
         inputPhone = (EditText) findViewById(R.id.inputPhone);
 
-        String number = "1234567890";
-        Log.i("Main Activity", number);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            number = PhoneNumberUtils.formatNumber(number, Locale.getDefault().getCountry());
-            Log.i("MainActivity", "Number format set new");
-        } else {
-            number = PhoneNumberUtils.formatNumber(number); //Deprecated method
-            Log.i("MainActivity", "Number format set old");
-        }
-        Log.i("Main Activity", number);
-
-        phoneWatcher = new TextWatcher() {
-
-            Boolean isPhoneEdit = false;
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-               if(!isPhoneEdit){
-                   isPhoneEdit = true;
-
-
-
-                   isPhoneEdit = false;
-               }
-            }
-        };
-        inputPhone.addTextChangedListener(phoneWatcher);
-
+        inputPhone.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
     }
 }

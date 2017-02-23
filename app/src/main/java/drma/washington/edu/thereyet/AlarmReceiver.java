09120d31f0bb3f95d65558f3618e804a5670862f
Duncan Andrew <duncanandrew@live.com>
2017-02-22 18:06:10 -0800

    fixed custom toasts

--- app/src/main/java/drma/washington/edu/thereyet/AlarmReceiver.java
+++ app/src/main/java/drma/washington/edu/thereyet/AlarmReceiver.java
@@ -5,6 +5,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -12,8 +16,6 @@ import android.widget.Toast;
  */
 
 public class AlarmReceiver extends BroadcastReceiver {
-    private AlarmApp app;
-    private MainActivity main;
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -22,7 +24,8 @@ public class AlarmReceiver extends BroadcastReceiver {
         String phoneNumber = bundle.getString("phoneNumber");
 
         Log.i("AlarmReciever", "Intent Recieved at " + System.currentTimeMillis());
-        Toast.makeText(context, phoneNumber + ": " + message, Toast.LENGTH_SHORT).show();
+        //Toast.makeText(context, phoneNumber + ": " + message, Toast.LENGTH_SHORT).show();
+        sendToast(message, phoneNumber, context);
         /**
          * Creating any more complicated toast seems to require access to the main activity
          * not sure how to do this yet, so I will hold off.
@@ -32,11 +35,28 @@ public class AlarmReceiver extends BroadcastReceiver {
          * http://stackoverflow.com/questions/16934425/call-an-activity-method-from-a-broadcastreceiver-class
          */
 
-        if(app == null){
-            Log.d("AlarmReciever", "Activity was null, creating it now");
-            app = (AlarmApp) context.getApplicationContext();
-        }
-
+        AlarmApp app = (AlarmApp) context.getApplicationContext();
         app.getAlarmManager().setAlarm();
+    }
+
+    /**
+     * Source for custom toast workaround
+     * http://stackoverflow.com/questions/1618800/show-complex-toast-from-broadcastreceiver
+     *
+     * @param context
+     */
+    public void sendToast(String message, String phoneNumber, Context context){
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View layout = inflater.inflate(R.layout.custom_toast, null);
+        TextView caption = (TextView) layout.findViewById(R.id.toast_caption);
+        TextView body = (TextView) layout.findViewById(R.id.toast_body);
+        caption.setText("Texting " + phoneNumber);
+        body.setText(message);
+
+        Toast toast = new Toast(context.getApplicationContext());
+        //toast.setGravity(Gravity.BOTTOM, 0, 0);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        toast.show();
     }
 }
